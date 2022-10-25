ff596eae7b986415fd66e5355c7f8484f674b14d
Josh Cobert <jdcobert@gmail.com>
2022-10-25 17:23:27 -0400

    set theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-primary": "#515CBB",
+        "theme-secondary": "#ADADAD",
+        "theme-tertiary": "#494949",
+      },
+      fontSize: {
+        'base': '1.05rem'
+      },
+      maxWidth: {
+        'layoutMax': '90rem'
+      },
+    },
   },
   plugins: [],
 };
