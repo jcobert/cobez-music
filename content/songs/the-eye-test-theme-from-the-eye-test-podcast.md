15186722d42d101b083d744bad2b3b237d84dced
Josh Cobert <jdcobert@gmail.com>
2022-10-26 21:56:54 -0400

    create basic page content layout

--- content/songs/the-eye-test-theme-from-the-eye-test-podcast.md
+++ content/songs/the-eye-test-theme-from-the-eye-test-podcast.md
@@ -2,10 +2,23 @@
 layout: music
 title: The Eye Test (Theme from The Eye Test Podcast)
 artist: Cobez
-date: 10/12/2022
+date: Apr 10, 2022
 artwork: /images/cms/theme-song-art.jpg
-description: The Eye Test is a sports podcast, hosted by my good friend Brian
-  Donovan. I currently produce the show - editing the episodes and prepping them
-  for release. This song is the extended version of the intro theme music that I
+description: >-
+  The Eye Test is a sports podcast, hosted by my good friend Brian Donovan. I
+  currently produce the show - editing the episodes and prepping them for
+  release. This song is the extended version of the intro theme music that I
   created for the show.
+
+
+  S﻿econd paragraph
+
+
+  > H﻿ere's a block quote
+
+
+  #### H﻿eading 4
+spotify: https://open.spotify.com/track/0gLGKcUMjGQQ8aimmgjtL8?si=c6ed19eddf234a51
+apple: https://music.apple.com/us/album/the-eye-test-theme-from-the-eye-test-podcast/1619215200?i=1619215201
+youtube: https://music.youtube.com/watch?v=R9kKrzz7imk
 ---
--- pages/music/[id].jsx
+++ pages/music/[id].jsx
@@ -1,4 +1,5 @@
 import { getAllSongIds, getSongData } from "../../lib/songs";
+import Image from "next/image";
 
 export async function getStaticPaths() {
   const paths = getAllSongIds();
@@ -20,17 +21,36 @@ export async function getStaticProps({ params }) {
 function Music({ songData }) {
   return (
     <div>
-      <div>
-        <img
-          src={songData.artwork}
-          alt="Album artwork"
-        />
+      <div className="flex flex-col md:flex-row gap-x-4 justify-center items-center">
+        {/* Album artwork */}
+        <div className="w-8/12 sm:w-6/12 md:w-4/12 max-w-xs p-4">
+          <Image
+            src={songData.artwork}
+            width={500}
+            height={500}
+            layout="responsive"
+            alt="Album artwork"
+          />
+        </div>
+        {/* Info and Links */}
+        <div>
+          {/* Title, Artist, Release Date */}
+          <div className="text-center py-4 px-2">
+            <h2 className="text-xl">{songData.title}</h2>
+            <h4 className="text-lg">{songData.artist}</h4>
+            <h6 className="text-xs">{songData.date}</h6>
+          </div>
+          {/* Stream Links */}
+          <div className="flex justify-around md:justify-evenly md:mt-4 p-2">
+            <a href={songData.spotify}>S</a>
+            <a href={songData.apple}>A</a>
+            <a href={songData.youtube}>Y</a>
+          </div>
+        </div>
       </div>
-      <h2>{songData.title}</h2>
-      <h3>{songData.artist}</h3>
-      <h4>{songData.date}</h4>
-      <div>
-        <p>{songData.description}</p>
+      {/* Description */}
+      <div className="md:w-10/12 xl:w-8/12 max-w-5xl p-4 mx-auto md:mt-4 lg:mt-12">
+        <div>{songData.description}</div>
       </div>
     </div>
   );
