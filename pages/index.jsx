6a80cf23d83a020a9730e58cc132957638443066
Josh Cobert <jdcobert@gmail.com>
2022-11-07 21:56:02 -0500

    add placeholder sections

--- pages/index.jsx
+++ pages/index.jsx
@@ -60,7 +60,43 @@ export default function Home({ songData }) {
                 <a className="inline-block rounded-md p-4 md:p-2 w-9/12 sm:max-w-[12rem] border border-theme-primary bg-white hover:bg-theme-primary text-theme-primary hover:text-white text-lg md:text-base font-bold transition">
                   <div className="flex gap-x-2 justify-center items-center">
                     <span>All Music</span>
-                    <FontAwesomeIcon className="animate-pulse" icon={faArrowRight} />
+                    <FontAwesomeIcon
+                      className="animate-pulse"
+                      icon={faArrowRight}
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+        {/* Section Break */}
+        <section className="">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-8 invisible">
+            <div className="py-8"></div>
+          </div>
+        </section>
+        {/* Section 2 */}
+        <section className="bg-theme-primary">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto pt-8 pb-12 md:pt-16 md:pb-20">
+            {/* Header */}
+            <div className="h-16 mx-auto max-w-3xl flex items-center justify-center md:justify-start md:pl-4 border-l-4 border-white border-b-4 rounded-bl-md mb-4 md:mb-12">
+              <h1 className="text-white text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
+                About
+              </h1>
+            </div>
+            <div className="w-full max-w-3xl mx-auto rounded-md shadow">
+              {/* Body */}
+              <div className="mx-auto bg-white border rounded p-4 md:p-8">
+                <div className="flex flex-col gap-y-8"></div>
+              </div>
+            </div>
+            {/* About Page Link */}
+            <div className="text-center mt-8 md:mt-12 py-2 md:pb-0">
+              <Link href="/about">
+                <a className="inline-block rounded-md p-4 md:p-2 w-9/12 sm:max-w-[12rem] border border-theme-primary bg-white hover:bg-theme-primary text-theme-primary hover:text-white text-lg md:text-base font-bold transition">
+                  <div className="flex gap-x-2 justify-center items-center">
+                    <span>Learn More...</span>
                   </div>
                 </a>
               </Link>
@@ -74,21 +110,20 @@ export default function Home({ songData }) {
           </div>
         </section>
         {/* Section 3 */}
-        <section className="bg-theme-primary">
-          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-28 invisible">
-            <div className="py-28"></div>
-          </div>
-        </section>
-        {/* Section Break */}
-        <section className="">
-          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-8 invisible">
-            <div className="py-8"></div>
-          </div>
-        </section>
-        {/* Section 4 */}
         <section className="bg-theme-secondary">
-          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-28 invisible">
-            <div className="py-28"></div>
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto pt-8 pb-12 md:pt-16 md:pb-20">
+            {/* Header */}
+            <div className="h-16 mx-auto max-w-3xl flex items-center justify-center md:justify-start md:pl-4 border-l-4 border-white border-b-4 rounded-bl-md mb-4 md:mb-12">
+              <h1 className="text-white text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
+                Contact Me
+              </h1>
+            </div>
+            <div className="w-full max-w-3xl mx-auto rounded-md shadow">
+              {/* Body */}
+              <div className="mx-auto bg-white border rounded p-4 md:p-8">
+                <div className="flex flex-col gap-y-8"></div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
