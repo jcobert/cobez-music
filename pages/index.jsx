04baf5bc4b600af77f870152ce55f7dd4dce12e5
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-11-07 21:56:21 -0500

    Improve homepage layout (#8)
    * structure sections
    
    * edit featured section spacing
    
    * animate music link
    
    * add placeholder sections

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import SongCard from "../components/SongCard";
 import { getAllSongData } from "../lib/songs";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowAltCircleRight } from "@fortawesome/free-regular-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home({ songData }) {
   const featuredSongs = [songData].flatMap((songs) => {
@@ -25,7 +25,7 @@ export default function Home({ songData }) {
       {/* Main */}
       <div>
         {/* Heading/Hero */}
-        <div className="">
+        <div className="px-10">
           <img
             className="mx-auto w-full max-w-xl"
             src="/images/cobez-logo.svg"
@@ -35,32 +35,93 @@ export default function Home({ songData }) {
             songwriter | producer
           </h4>
         </div>
-        {/* Featured Music */}
-        <section>
-          <div className="my-20 md:mt-28 lg:mt-32 w-11/12 mx-auto">
+        {/* Featured Music Section */}
+        <section className="bg-gray-100">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto pt-8 pb-12 md:pt-16 md:pb-20">
             {/* Header */}
-            <div className="h-16 mx-auto max-w-3xl rounded-t-md bg-theme-primary border border-b-0 border-theme-secondary flex items-center justify-center md:justify-start md:pl-4">
-              <h1 className="text-white text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
+            <div className="h-16 mx-auto max-w-3xl flex items-center justify-center md:justify-start md:pl-4 border-l-4 border-theme-primary border-b-4 rounded-bl-md mb-4 md:mb-12">
+              <h1 className="text-theme-primary text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
                 Featured Music
               </h1>
             </div>
-            {/* Body */}
-            <div className="mx-auto max-w-3xl bg-theme-tertiary/90 border border-t-0 border-theme-secondary rounded-b p-4 md:p-8">
-              <div className="flex flex-col gap-y-8">
-                {featuredSongs.map((item) => (
-                  <SongCard song={item} />
-                ))}
+            <div className="w-fit mx-auto rounded-md shadow">
+              {/* Body */}
+              <div className="mx-auto max-w-3xl bg-white border rounded p-4 md:p-8">
+                <div className="flex flex-col gap-y-8">
+                  {featuredSongs.map((item) => (
+                    <SongCard song={item} />
+                  ))}
+                </div>
               </div>
-              {/* Music Page Link */}
-              <div className="text-center mt-6 md:mt-2 py-4 md:pb-0">
-                <Link href="/music">
-                  <a className="inline-block rounded-md p-4 md:p-2 w-9/12 sm:max-w-[12rem] border border-theme-primary bg-white hover:bg-theme-primary text-theme-primary hover:text-white text-lg md:text-base font-bold transition">
-                    <div className="flex gap-x-2 justify-center items-center">
-                      <span>All Music</span>
-                      <FontAwesomeIcon icon={faArrowAltCircleRight} />
-                    </div>
-                  </a>
-                </Link>
+            </div>
+            {/* Music Page Link */}
+            <div className="text-center mt-8 md:mt-12 py-2 md:pb-0">
+              <Link href="/music">
+                <a className="inline-block rounded-md p-4 md:p-2 w-9/12 sm:max-w-[12rem] border border-theme-primary bg-white hover:bg-theme-primary text-theme-primary hover:text-white text-lg md:text-base font-bold transition">
+                  <div className="flex gap-x-2 justify-center items-center">
+                    <span>All Music</span>
+                    <FontAwesomeIcon
+                      className="animate-pulse"
+                      icon={faArrowRight}
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+        {/* Section Break */}
+        <section className="">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-8 invisible">
+            <div className="py-8"></div>
+          </div>
+        </section>
+        {/* Section 2 */}
+        <section className="bg-theme-primary">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto pt-8 pb-12 md:pt-16 md:pb-20">
+            {/* Header */}
+            <div className="h-16 mx-auto max-w-3xl flex items-center justify-center md:justify-start md:pl-4 border-l-4 border-white border-b-4 rounded-bl-md mb-4 md:mb-12">
+              <h1 className="text-white text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
+                About
+              </h1>
+            </div>
+            <div className="w-full max-w-3xl mx-auto rounded-md shadow">
+              {/* Body */}
+              <div className="mx-auto bg-white border rounded p-4 md:p-8">
+                <div className="flex flex-col gap-y-8"></div>
+              </div>
+            </div>
+            {/* About Page Link */}
+            <div className="text-center mt-8 md:mt-12 py-2 md:pb-0">
+              <Link href="/about">
+                <a className="inline-block rounded-md p-4 md:p-2 w-9/12 sm:max-w-[12rem] border border-theme-primary bg-white hover:bg-theme-primary text-theme-primary hover:text-white text-lg md:text-base font-bold transition">
+                  <div className="flex gap-x-2 justify-center items-center">
+                    <span>Learn More...</span>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          </div>
+        </section>
+        {/* Section Break */}
+        <section className="">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto py-8 invisible">
+            <div className="py-8"></div>
+          </div>
+        </section>
+        {/* Section 3 */}
+        <section className="bg-theme-secondary">
+          <div className="mt-20 md:mt-24 lg:mt-24 w-11/12 mx-auto pt-8 pb-12 md:pt-16 md:pb-20">
+            {/* Header */}
+            <div className="h-16 mx-auto max-w-3xl flex items-center justify-center md:justify-start md:pl-4 border-l-4 border-white border-b-4 rounded-bl-md mb-4 md:mb-12">
+              <h1 className="text-white text-3xl md:text-3xl text-center md:text-left font-bellotaHeading font-bold">
+                Contact Me
+              </h1>
+            </div>
+            <div className="w-full max-w-3xl mx-auto rounded-md shadow">
+              {/* Body */}
+              <div className="mx-auto bg-white border rounded p-4 md:p-8">
+                <div className="flex flex-col gap-y-8"></div>
               </div>
             </div>
           </div>
