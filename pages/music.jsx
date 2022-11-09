d0bfbbb6fff138b3ce91988304a7045819624d90
Josh Cobert <jdcobert@gmail.com>
2022-11-09 18:32:22 -0500

    pass viewtype state to songcard

--- pages/music.jsx
+++ pages/music.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import SongCard from "../components/SongCard";
 import Heading from "../components/Heading";
@@ -6,6 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList, faGrip } from "@fortawesome/free-solid-svg-icons";
 
 export default function Music({ songData }) {
+  const [viewType, setViewType] = useState("list");
   const featuredSongs = [songData].flatMap((songs) => {
     const songJson = JSON.parse(songs);
     return songJson;
@@ -27,11 +29,11 @@ export default function Music({ songData }) {
         <div className="mx-auto max-w-3xl border-theme-secondary rounded-md px-4 md:p-0">
           {/* Sort and Filter */}
           <div className="flex justify-end pb-4">
-            <div className="flex justify-center items-center text-2xl gap-x-4">
-              <button>
+            <div className="flex justify-center items-center text-2xl text-theme-tertiary gap-x-4">
+              <button onClick={() => setViewType("list")}>
                 <FontAwesomeIcon icon={faList} />
               </button>
-              <button>
+              <button onClick={() => setViewType("grid")}>
                 <FontAwesomeIcon icon={faGrip} />
               </button>
             </div>
@@ -39,7 +41,7 @@ export default function Music({ songData }) {
           {/* Songs */}
           <div className="flex flex-col gap-y-8">
             {featuredSongs.map((item, i) => (
-              <SongCard key={i} song={item} />
+              <SongCard key={i} song={item} view={viewType} />
             ))}
           </div>
         </div>
