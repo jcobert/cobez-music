465c9e7cd0f66f873dc9d527ddf3a95a4422b791
Josh Cobert <jdcobert@gmail.com>
2022-11-09 09:43:16 -0500

    add view type buttons

--- pages/music.jsx
+++ pages/music.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import SongCard from "../components/SongCard";
 import Heading from "../components/Heading";
 import { getAllSongData } from "../lib/songs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faList, faGrip } from "@fortawesome/free-solid-svg-icons";
 
 export default function Music({ songData }) {
   const featuredSongs = [songData].flatMap((songs) => {
@@ -20,9 +22,21 @@ export default function Music({ songData }) {
       <section>
         <Heading className="w-11/12 max-w-3xl mx-auto" text="Music" />
       </section>
-      {/* Songs */}
+      {/* Main Content */}
       <section className="mt-8 md:mt-12 lg:mt-12 mx-auto bg-white py-4 pb-12 md:py-16">
         <div className="mx-auto max-w-3xl border-theme-secondary rounded-md px-4 md:p-0">
+          {/* Sort and Filter */}
+          <div className="flex justify-end pb-4">
+            <div className="flex justify-center items-center text-2xl gap-x-4">
+              <button>
+                <FontAwesomeIcon icon={faList} />
+              </button>
+              <button>
+                <FontAwesomeIcon icon={faGrip} />
+              </button>
+            </div>
+          </div>
+          {/* Songs */}
           <div className="flex flex-col gap-y-8">
             {featuredSongs.map((item, i) => (
               <SongCard key={i} song={item} />
