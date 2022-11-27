9e252cbb5a7dd4b0a28c220ebdd86dff392e13cf
Josh Cobert <35186740+jcobert@users.noreply.github.com>
2022-11-27 18:31:38 -0500

    Contact page (#14)
    * create base form layout
    
    * move contact form to own component
    
    * add form submission handling
    
    * add CTA
    
    * use stateful form handling
    
    * adjust styling

---
+++ components/ContactForm.jsx
@@ -0,0 +1,145 @@
+import { useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+
+export default function ContactForm(props) {
+  const [submitted, setSubmitted] = useState(false);
+  const [inputValues, setInputValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    setInputValues({
+      ...inputValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...inputValues }),
+    };
+    fetch("/", options)
+      .then(function (res) {})
+      .catch(function (error) {});
+
+    setSubmitted(true);
+    setInputValues({
+      firstName: "",
+      lastName: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} name="contact" data-netlify="true">        
+        <div className="border rounded shadow-sm text-lg px-4 pt-8 pb-12 md:px-16 lg:px-48">
+          {/* Body */}
+          <div className="grid grid-cols-2 gap-6">
+            <div className="col-span-2 mb-4 md:mb-8">
+              <h3 className="text-center text-3xl md:text-4xl font-bold text-theme-primary">
+                Send me a message!
+              </h3>
+            </div>
+            {/* First */}
+            <div className="col-span-2 md:col-span-1">
+              <label htmlFor="first-name" className="block">
+                First Name
+              </label>
+              <input
+                onChange={handleInputChange}
+                type="text"
+                name="firstName"
+                id="first-name"
+                value={inputValues.firstName}
+                required
+                className="border rounded w-full px-2 py-1 shadow-sm"
+              />
+            </div>
+            {/* Last */}
+            <div className="col-span-2 md:col-span-1">
+              <label htmlFor="last-name" className="block">
+                Last Name
+              </label>
+              <input
+                onChange={handleInputChange}
+                type="text"
+                name="lastName"
+                id="last-name"
+                value={inputValues.lastName}
+                required
+                className="border rounded w-full px-2 py-1 shadow-sm"
+              />
+            </div>
+            {/* Email */}
+            <div className="col-span-2">
+              <label htmlFor="email" className="block">
+                Email
+              </label>
+              <input
+                onChange={handleInputChange}
+                type="email"
+                name="email"
+                id="email"
+                value={inputValues.email}
+                required
+                className="border rounded w-full px-2 py-1 shadow-sm"
+              />
+            </div>
+            {/* Message */}
+            <div className="col-span-2">
+              <label htmlFor="message" className="block">
+                Message
+              </label>
+              <textarea
+                onChange={handleInputChange}
+                name="message"
+                id="message"
+                value={inputValues.message}
+                required
+                rows={4}
+                className="border rounded w-full px-2 py-1 shadow-sm"
+              ></textarea>
+            </div>
+            {/* Submit */}
+            <div className="col-span-2 w-full mt-8 md:mt-12">
+              <button
+                type="submit"
+                className={`block rounded-md mx-auto p-4 md:p-3 w-9/12 sm:max-w-[12rem] border bg-theme-primary hover:bg-theme-tertiary text-white hover:text-white font-bold transition ${
+                  submitted ? "hidden" : ""
+                }`}
+              >
+                <span>Submit</span>
+              </button>
+              {/* Success Response */}
+              <div
+                className={`flex justify-center items-center gap-x-2 text-center mx-auto p-4 md:p-3 w-full text-theme-primary border border-transparent ${
+                  !submitted ? "hidden" : ""
+                }`}
+              >
+                <FontAwesomeIcon icon={faCircleCheck} />
+                <span>Your message was sent!</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import Heading from "../components/Heading";
+import ContactForm from "../components/ContactForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Contact() {
+  const links = {
+    instagram: "https://www.instagram.com/jcobez",
+    email: "mailto:josh@cobezmusic.com",
+  };
+
   return (
     <div className="bg-theme-primary">
       <Head>
@@ -14,8 +23,39 @@ export default function Contact() {
         <Heading className="w-11/12 max-w-3xl mx-auto" text="Contact" />
       </section>
       {/* Body */}
-      <section className="mt-8 md:mt-12 lg:mt-12 mx-auto bg-white py-4 pb-12 md:py-16 h-screen">
-        <div className="mx-auto max-w-3xl border rounded-md px-4 md:p-0"></div>
+      <section className="wrapper-body bg-white">
+        <div className="container-body">
+          {/* Graphic */}
+          <div className="text-center text-7xl md:text-8xl text-theme-primary my-8 md:my-2">
+            <FontAwesomeIcon icon={faVolumeHigh} />
+          </div>
+          {/* CTA */}
+          <div className="my-8 md:my-12 max-w-3xl mx-auto text-center">
+            <p>Looking to collaborate or have something to share?</p>
+            <p>Please leave a message below.</p>
+            <p className="mt-4">
+              Feel free to reach out on Instagram or send me an email as well.
+            </p>
+            <div className="text-theme-primary text-4xl md:text-3xl flex justify-center items-center gap-14 md:gap-8 py-6 pt-8">
+              <a
+                class="hover:text-theme-tertiary transition-all"
+                href={links.instagram}
+              >
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+              <a
+                class="hover:text-theme-tertiary transition-all"
+                href={links.email}
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
+            </div>
+          </div>
+          {/* Form */}
+          <div>
+            <ContactForm />
+          </div>
+        </div>
       </section>
     </div>
   );
